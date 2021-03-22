87ffe8ae060734aefe5b7675a45aedae69fac4b4
Piero Dondero <33132220+winemago@users.noreply.github.com>
2021-03-21 23:28:03 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.listen(3000, ()=> console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json());
 
+const array_data = [];
+
 app.post('/api', (request,response)=>{
     console.log('request: ');
-    console.log(request.body);
+    
+    array_data.push(request.body);
+    console.log(array_data);
+
     //response.end();                           allways complete a request
     response.json({
         status: 'success',
