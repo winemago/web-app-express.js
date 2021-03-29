c837e52053cd3e692609c22091d6b404709bf443
Piero Dondero <33132220+winemago@users.noreply.github.com>
2021-03-29 00:23:02 -0300

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -3,6 +3,15 @@ function setup(){
     const video = createCapture(VIDEO);
     video.size(200,200);
 
+    const mymap = L.map('mapid').setView([0,0], 2);
+
+    const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(mymap);
+
+    world();
+
     document.getElementById('geolocate').addEventListener('click', event => {
         if(!navigator.geolocation) {
             status.textContent = 'Geolocation is not supported by your browser';
@@ -30,39 +39,32 @@ function setup(){
                 const response = await fetch('/api',options);            //in order to send a POST I need a second argument in fetch, a JSON.
                 const dat = await response.json();
                 console.log(dat);  
-
-
-                drawmap(lat,lon,username,image64);        
+                
+                const perfil = L.icon({
+                    iconUrl: image64,
+                    iconSize: [60, 42],
+                    iconAnchor: [25, 16],
+                    popupAnchor:  [2, -15]
+                  });
+                const marker = L.marker([lat, lon], { icon: perfil }).addTo(mymap);
             });
         }
-        function drawmap(lat,long,username,image64){
-            //init map
-            const mymap = L.map('mapid').setView([0,0], 2);
-
-            const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-            const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            const tiles = L.tileLayer(tileUrl, { attribution });
-            tiles.addTo(mymap);
-
-
+    }); 
+    
+    async function world(){
+        const response = await fetch('/api');
+        const data = await response.json();
+        console.log(data);
+        
+        data.data.forEach(item => {
             const perfil = L.icon({
-                iconUrl: image64,
+                iconUrl: item.image64,
                 iconSize: [60, 42],
                 iconAnchor: [25, 16],
                 popupAnchor:  [2, -15]
               });
-            const marker = L.marker([lat, long], { icon: perfil }).addTo(mymap);
-            marker.bindPopup(`Hi there ${username}!`).openPopup();
-              
-            /*const circle = L.circle([lat, long], {
-                color: 'red',
-                fillColor: '#f03',
-                fillOpacity: 0.5,
-                radius: 500
-            }).addTo(mymap);
-            circle.*/
-        }
-    }); 
-    
-
+            const marker = L.marker([item.lat, item.lon], { icon: perfil }).addTo(mymap);
+        });   
+    }
 } 
+
